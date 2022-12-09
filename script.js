8e79ba81358e6cfed440acd65b9a933c638d30fa
Partha Mondal <partha2602mondal@gmail.com>
2022-12-10 01:03:29 +0530

    new

---
+++ script.js
@@ -0,0 +1,27 @@
+let calValue = '';
+let calculatorResult = document.getElementById('calculatorResult');
+
+let bgButton = document.querySelectorAll('button');
+Array.from(bgButton).forEach((calButton)=>{
+
+    calButton.addEventListener("click", ()=>{
+
+        if(calButton.innerHTML == '='){
+            calValue = eval(calValue);
+            calculatorResult.value = calValue;
+        }
+
+        else if(calButton.innerHTML == 'CE'){
+            calValue = '';
+            calculatorResult.value = calValue;
+        }
+        else if(calButton.innerHTML == 'DEL'){
+            calValue = calculatorResult.value.slice(0,-1);
+            calculatorResult.value = calValue;
+        }
+        else{
+        calValue = calValue + calButton.innerHTML;
+        calculatorResult.value = calValue;
+        }
+    })
+});
